bb6aa082bd4cd203451da6a304b1ac8003f43344
Prakash Adhikari <pa7846665@gmail.com>
2025-08-19 11:10:13 +0530

    module 3

---
+++ class03.py
@@ -0,0 +1,35 @@
+# a=[1,2,3]
+# b=[1,2,3]
+
+# print(a==b)
+# print(a is b)
+
+# x=10
+# if x>0:
+#     print('x is is a postivive number ')
+# elif x<0:
+#     print('x is a negative number')
+# else:
+#     print('x must be zero')
+    
+    
+#     # return  x%2 ? 1 : 0
+    
+# # result = "even"
+
+# # fruit=['apple', 'banana', 'cherry']
+
+# for i in range(5):
+#     print(i, end=' ')
+    
+    
+# print()
+
+# for fruit in ['apple', 'banana', 'cherry']:
+#     print(fruit, end=' ')
+    
+    
+count = 0
+while count <5:
+    print(count, end=' ')
+    count += 1
